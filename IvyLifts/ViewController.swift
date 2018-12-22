6fc1c349ac37356cc1997a0c6c59c98c823359a1
Mike Lin Chu <mikelin1557@gmail.com>
2018-12-21 23:57:28 -0800

    Initial commit

---
+++ IvyLifts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IvyLifts
+//
+//  Created by Mike Chu on 12/21/18.
+//  Copyright © 2018 Mike Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+target 'IvyLifts' do
+  use_frameworks!
+  pod 'Alamofire'
+  pod 'RealmSwift'
+
+end
